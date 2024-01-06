875827e501acfcf571bca0ca6f544f902b3e89bd
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-01-06 02:15:39 -0800

    completed register page

--- lib/modules/auth/pages/register_page.dart
+++ lib/modules/auth/pages/register_page.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_grocery_store/helper/widgets/button_widget.dart';
 import 'package:flutter_grocery_store/helper/widgets/text_field_widget.dart';
+import 'package:flutter_grocery_store/modules/auth/pages/login_page.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
+
 
   @override
   Widget build(BuildContext context) {
@@ -13,31 +17,69 @@ class RegisterPage extends StatelessWidget {
           width: double.infinity,
           child: Padding(
             padding: const EdgeInsets.all(25),
-            child: Column(
-              children: [
-                // logo and welcome text
-                Image.asset("assets/images/logo_horiz.png",height: 48),
-                const SizedBox(height: 10),
-                const Text("به فروشگاه آنلاین خودتون خوش اومدید",style: TextStyle(
-                  fontSize: 16,fontWeight: FontWeight.bold,color: Color(0xFF8C8C8C)
-                ),),
-                const SizedBox(height: 30),
-                // register form
-                const Align(
-                    alignment: Alignment.topRight,
-                    child: Text("ثبت نام",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold))),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  // logo and welcome text
+                  Image.asset("assets/images/logo_horiz.png",height: 48),
+                  const SizedBox(height: 10),
+                  const Text("به فروشگاه آنلاین خودتون خوش اومدید",style: TextStyle(
+                    fontSize: 16,fontWeight: FontWeight.bold,color: Color(0xFF8C8C8C)
+                  ),),
+                  const SizedBox(height: 30),
+                  // register form
+                  const Align(
+                      alignment: Alignment.topRight,
+                      child: Text("ثبت نام",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold))),
+                  const SizedBox(height: 10),
+                  // full name
+                  const TextFieldWidget(
+                    hintText: "نام و نام خانوادگی",
+                    type: TextInputType.phone,
+                    icon: Iconsax.user,
+                  ),
+              
+                  const SizedBox(height: 15),
+                  //mobile
+                  const TextFieldWidget(
+                    hintText: "شماره موبایل",
+                    type: TextInputType.phone,
+                    icon: Iconsax.mobile,
+                  ),
+                  const SizedBox(height: 15),
+                  // password
+                  const TextFieldWidget(
+                    hintText: "رمزعبور",
+                    type: TextInputType.visiblePassword,
+                  ),
+                  const SizedBox(height: 15),
+                  // repeat pass
+                  const TextFieldWidget(
+                    hintText: "تکرار رمز عبور",
+                    type: TextInputType.visiblePassword,
+                  ),
+                  const SizedBox(height: 25),
+              
+                  // register button
+                  ButtonWidget(
+                      onPress: () {},
+                      text: "ثبت نام"),
+                  const SizedBox(height: 25),
+                  // login btn
+                   Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("حساب کاربری دارید؟ ",style: TextStyle(color: Color(0xFF8C8C8C))),
+                      const SizedBox(width: 5),
+                      GestureDetector(
+                          onTap: () => Get.to(const LoginPage()),
+                          child: Text("وارد شوید",style: TextStyle(color: Theme.of(context).primaryColor,fontWeight: FontWeight.bold))),
+                    ],
+                  )
 
-                const TextFieldWidget(
-                  hintText: "شماره موبایل",
-                  type: TextInputType.phone,
-                  icon: Iconsax.call,
-                ),
-                const SizedBox(height: 20),
-                const TextFieldWidget(
-                  hintText: "رمزعبور",
-                  type: TextInputType.visiblePassword,
-                )
-              ],
+
+                ],
+              ),
             ),
           ),
         ),
