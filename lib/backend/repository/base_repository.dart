4c44c3dd993bf83e7d06f98c2590f6c081bc4f3e
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-01-06 21:32:04 -0800

    add validate status and headers to dio BaseRepository

--- lib/backend/repository/base_repository.dart
+++ lib/backend/repository/base_repository.dart
@@ -1,5 +1,11 @@
 import 'package:dio/dio.dart';
 
 class BaseRepository {
-  final Dio dio = Dio(BaseOptions(baseUrl: 'https://shop.hitaldev.ir/api'));
+  final Dio dio = Dio(BaseOptions(
+      baseUrl: 'https://shop.hitaldev.ir/api',
+      validateStatus: (status) => status! < 500,
+      headers: {
+        "Accept" : "application/json"
+      }
+  ));
 }
