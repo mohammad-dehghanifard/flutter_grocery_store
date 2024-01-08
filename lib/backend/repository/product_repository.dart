a9da8e5a9e49ff66ed5aeba74a1876fa3dc2a64b
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-01-08 05:17:12 -0800

    impl getAllCategories method in ProductRepository class

--- lib/backend/repository/product_repository.dart
+++ lib/backend/repository/product_repository.dart
@@ -1,11 +1,17 @@
 import 'package:flutter_grocery_store/backend/repository/base_repository.dart';
+import 'package:flutter_grocery_store/backend/response/category_response.dart';
 import 'package:flutter_grocery_store/backend/response/dash_board_response.dart';
 
 class ProductRepository extends BaseRepository {
 
-  Future<DashboardResponse?> getDashboardData() async {
+  Future<DashboardResponse> getDashboardData() async {
     var response = await dio.get("/dashboard");
     return DashboardResponse.fromJson(response.data);
+  }
+
+  Future<CategoryResponse> getAllCategories() async {
+    final response = await dio.get("/categories");
+    return CategoryResponse.fromJson(response.data);
   }
 
 }
