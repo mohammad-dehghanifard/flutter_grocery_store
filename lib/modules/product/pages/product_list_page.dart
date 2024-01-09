af222838b847c315d12f0a18dd3ecee1697ad3c2
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-01-08 22:49:50 -0800

    impl ProductListFilterMenuWidget custom widget

--- lib/modules/product/pages/product_list_page.dart
+++ lib/modules/product/pages/product_list_page.dart
@@ -3,9 +3,9 @@ import 'package:flutter_grocery_store/helper/widgets/appbar_widget.dart';
 import 'package:flutter_grocery_store/modules/product/controller/product_list_controller.dart';
 import 'package:flutter_grocery_store/modules/product/widgets/category_text_list_widget.dart';
 import 'package:flutter_grocery_store/modules/product/widgets/grid_product_list_widget.dart';
+import 'package:flutter_grocery_store/modules/product/widgets/product_list_filter_menu_widget.dart';
 import 'package:flutter_grocery_store/modules/product/widgets/product_list_search_widget.dart';
 import 'package:get/get.dart';
-import 'package:iconsax/iconsax.dart';
 
 class ProductListPage extends StatelessWidget {
   const ProductListPage({super.key});
@@ -31,63 +31,7 @@ class ProductListPage extends StatelessWidget {
                     ),
                     const SizedBox(width: 20),
                     // filter
-                    PopupMenuButton<Sort>(
-                        onSelected: (sort) => controller.sort(sort),
-                        position: PopupMenuPosition.under,
-                        color: Colors.white,
-                        surfaceTintColor: Colors.transparent,
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-                        itemBuilder: (context) {
-                          return [
-                             PopupMenuItem(
-                                 value: Sort(orderColumn:"id",orderType: "DESC"),
-                                 child: Container(
-                               padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
-                                decoration: const BoxDecoration(
-                                  border: Border(bottom: BorderSide(color: Color(0xFFDEDEDE),width: 1))
-                                ),
-                                child: const Center(child: Text("جدیدترین ها")))),
-                             PopupMenuItem(
-                                  value: Sort(orderColumn:"discount",orderType: "DESC"),
-                                 child: Container(
-                               padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
-                                decoration: const BoxDecoration(
-                                  border: Border(bottom: BorderSide(color: Color(0xFFDEDEDE),width: 1))
-                                ),
-                                child: const Center(child: Text("بیشترین تخفیف")))),
-                             PopupMenuItem(
-                                  value: Sort(orderColumn: "price",orderType: "Asc"),
-                                 child: Container(
-                               padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
-                                decoration: const BoxDecoration(
-                                  border: Border(bottom: BorderSide(color: Color(0xFFDEDEDE),width: 1))
-                                ),
-                                child: const Center(child: Text("ارزانترین")))),
-                             PopupMenuItem(
-                                 value: Sort(orderColumn:"price",orderType: "DESC"),
-                                 child: Container(
-                               padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
-                                child: const Center(child: Text("گرانترین")))),
-                          ];
-                        },
-                        child: Container(
-                          padding: const EdgeInsets.all(7),
-                          margin: const EdgeInsets.only(left: 20),
-                          width: 40,
-                          height: 40,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Colors.white,
-                              boxShadow: [
-                                BoxShadow(
-                                    color: const Color(0xFFEA5E24).withOpacity(0.19),
-                                    blurRadius: 4,
-                                    offset: const Offset(0,1)
-                                )
-                              ]
-                          ),
-                          child: const Icon(Iconsax.sort),
-                        ))
+                    ProductListFilterMenuWidget(controller: controller),
 
                   ],
                 ),
