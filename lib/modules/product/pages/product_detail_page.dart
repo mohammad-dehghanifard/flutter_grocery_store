20a4ad4df04c17f9a663d3f557490697ec145ddc
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-01-09 05:55:55 -0800

    impl ProductSliderWidget custom widget

--- lib/modules/product/pages/product_detail_page.dart
+++ lib/modules/product/pages/product_detail_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_grocery_store/helper/widgets/app_bar_button_widget.dart';
 import 'package:flutter_grocery_store/helper/widgets/appbar_widget.dart';
+import 'package:flutter_grocery_store/modules/product/widgets/product_slider_widget.dart';
 import 'package:iconsax/iconsax.dart';
 
 class ProductDetailPage extends StatelessWidget {
@@ -13,6 +14,11 @@ class ProductDetailPage extends StatelessWidget {
         child: Column(
           children: [
             AppBarWidget(action: AppBarButtonWidget(icon: Iconsax.heart,onTap: (){})),
+            ProductSliderWidget(
+                imageList: [
+                  'https://dl.hitaldev.com/ecommerce/product_images/412286.jpg',
+                  'https://dl.hitaldev.com/ecommerce/product_images/321174.jpg'
+                ])
           ],
         ),
       ),
---
+++ lib/modules/product/widgets/product_slider_widget.dart
@@ -0,0 +1,64 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+class ProductSliderWidget extends StatefulWidget {
+  const ProductSliderWidget({super.key, required this.imageList});
+  final List<String> imageList;
+
+
+  @override
+  State<ProductSliderWidget> createState() => _ProductSliderWidgetState();
+}
+
+class _ProductSliderWidgetState extends State<ProductSliderWidget> {
+  int selectedImage = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: [
+        CarouselSlider.builder(
+          itemCount: widget.imageList.length,
+          options: CarouselOptions(
+            height: 420,
+            viewportFraction: 1,
+            onPageChanged: (index, reason) {
+              setState(() {
+                selectedImage = index;
+              });
+            },
+            autoPlay: true,
+            autoPlayInterval: const Duration(seconds: 4),
+          ),
+          itemBuilder: (BuildContext context, int itemIndex, int pageViewIndex) {
+            return SizedBox(
+              width: double.infinity,
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.network(widget.imageList[itemIndex],fit: BoxFit.cover,)),
+            );
+          },
+        ),
+        // indicator
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(widget.imageList.length, (index) {
+              return AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                width: selectedImage == index? 25 : 8,
+                height:  8,
+                margin: const EdgeInsets.symmetric(horizontal: 4),
+                decoration:  BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: selectedImage == index? Theme.of(context).primaryColor : const Color(0xFFD2D2D2)
+                ),
+              );
+            }) ,
+          ),
+        )
+      ],
+    );
+  }
+}
