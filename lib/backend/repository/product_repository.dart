824e1c201f7ab781d8ec34dab2f8b6c5b0e30056
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-01-09 21:10:20 -0800

    impl getProductDetail api call method in ProductRepository

--- lib/backend/repository/product_repository.dart
+++ lib/backend/repository/product_repository.dart
@@ -1,20 +1,22 @@
+import 'package:flutter_grocery_store/backend/models/product.dart';
 import 'package:flutter_grocery_store/backend/repository/base_repository.dart';
 import 'package:flutter_grocery_store/backend/response/category_response.dart';
 import 'package:flutter_grocery_store/backend/response/dash_board_response.dart';
 import 'package:flutter_grocery_store/backend/response/product_response.dart';
 
 class ProductRepository extends BaseRepository {
-
+  
+  // home api
   Future<DashboardResponse> getDashboardData() async {
     var response = await dio.get("/dashboard");
     return DashboardResponse.fromJson(response.data);
   }
-
+  // all category
   Future<CategoryResponse> getAllCategories() async {
     final response = await dio.get("/categories");
     return CategoryResponse.fromJson(response.data);
   }
-
+  // filter product
   Future<ProductResponse> filterProducts({int? categoryId, String? keyWord,String? orderColumn,String? orderType}) async {
     Map<String,dynamic> queryPrams = {};
     //#region check queryParameters
@@ -33,6 +35,12 @@ class ProductRepository extends BaseRepository {
     //#endregion
     final response = await dio.get("/products",queryParameters: queryPrams);
     return ProductResponse.fromJson(response.data);
+  }
+  // get product detail
+  Future<Product> getProductDetail({required int id}) async {
+    final response = await dio.get("/products/$id");
+    print(response.data['data'].toString());
+    return Product.fromJson(response.data['data']);
   }
 
 }
