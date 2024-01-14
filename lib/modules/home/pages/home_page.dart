7d582be74171c030334692c16c6abeca8884a15a
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-01-14 00:03:12 -0800

    impl BookMarkPage ui

--- lib/modules/home/pages/home_page.dart
+++ lib/modules/home/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_grocery_store/modules/home/controller/home_controller.da
 import 'package:flutter_grocery_store/modules/home/pages/dashboard_page.dart';
 import 'package:flutter_grocery_store/modules/home/widgets/button_navigation.dart';
 import 'package:flutter_grocery_store/modules/home/widgets/home_app_bar_widget.dart';
+import 'package:flutter_grocery_store/modules/profile/pages/bookmark_page.dart';
 import 'package:flutter_grocery_store/modules/profile/pages/profile_page.dart';
 import 'package:get/get.dart';
 
@@ -31,7 +32,7 @@ class HomePage extends StatelessWidget {
                     const DashboardPage(),
                     const CategoryPage(),
                     Container(color: Colors.blue),
-                    Container(color: Colors.orange),
+                    const BookMarkPage(),
                     const ProfilePage()
                   ],
                 )),
